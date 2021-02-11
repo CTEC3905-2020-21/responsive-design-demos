4746f9875e16623ab107b68323dff4ca44ab0dbd
Graeme Stuart <ggstuart@gmail.com>
2021-02-11 21:16:06 +0000

    kitten demo

---
+++ demo/scripts.js
@@ -0,0 +1,9 @@
+menuToggler.addEventListener('click', ev => {
+	menuToggler.classList.toggle('open');
+});
+
+for (const element of document.querySelectorAll('nav a')) {
+	element.addEventListener('click', ev => {
+		menuToggler.classList.remove('open');
+	});
+}
